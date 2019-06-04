7bbf50c30729e4a5d2ffc803aa803b587863c664
Reza Soodin <rsoodin@ncb.mu>
2019-06-04 14:15:34 +0400

    Update settings.py

--- openshift_django/settings.py
+++ openshift_django/settings.py
@@ -79,7 +79,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mydjango',
         'USER': 'myroot',
         'PASSWORD': 'myroot123',
